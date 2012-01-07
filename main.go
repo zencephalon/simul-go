731e2fa089915385264d207a689d9cd303a87252
Matthias Bundai <zenceph@gmail.com>
2012-01-06 19:49:59 -0600

    Fleshing out the API

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
    "fmt"
+   "strconv"
 )
 
 type Space struct {
@@ -38,7 +39,7 @@ func (p *Board) Print() {
    f := func(s Space) (o string) {
       switch s.color {
       case 0:
-         o = "."
+         o = strconv.Itoa(s.freedom)
       case 1:
          o = "x"
       case 2:
@@ -55,18 +56,47 @@ func (p *Board) Print() {
 }
 
 func (p *Board) ValidMove(r, c int) bool {
-   return !((r < 0 || r >= p.h) || (c < 0 || c >= p.w)) && p.spaces[r][c].color == 0 && p.spaces[r][c].freedom != 0
+   return p.InBoundary(r, c) && p.spaces[r][c].color == 0 && p.spaces[r][c].freedom != 0
 }
 
-func GetMove() (r, c int) {
-   fmt.Println("Please enter a move: ")
-   fmt.Scanf("%d,%d", &r, &c)
+func (p *Board) InBoundary(r, c int) bool {
+   return !((r < 0 || r >= p.h) || (c < 0 || c >= p.w))
+}
+
+func (p *Board) GetMove() (r, c int) {
+   r, c = -1, -1
+   for !p.ValidMove(r, c) {
+      fmt.Println("Please enter a move: (int, int)")
+      fmt.Scanf("%d,%d", &r, &c)
+   }
    return r, c
+}
+
+// Assumes we are given a valid move.
+func (p *Board) PlaceColor(r, c, color int) {
+   p.spaces[r][c].color = color
+   p.AdjustFreedoms(r, c, color)
+}
+
+func (p *Board) AdjustFreedoms(r, c, color int) {
+   p.SetFreedom(r - 1, c, -1)
+   p.SetFreedom(r + 1, c, -1)
+   p.SetFreedom(r, c - 1, -1)
+   p.SetFreedom(r, c + 1, -1)
+}
+
+func (p *Board) SetFreedom(r, c, freedom int) {
+   if p.InBoundary(r, c) {
+      p.spaces[r][c].freedom += freedom
+   }
 }
 
 func main() {
    board := new(Board).Init(10, 10)
+   board.PlaceColor(5, 5, 1)
    board.Print()
-   r, c := GetMove()
-   fmt.Printf("Got two numbers %d, %d", r, c)
+   r, c := board.GetMove()
+   board.PlaceColor(r, c, 1)
+   fmt.Printf("Got two numbers %d, %d\n", r, c)
+   board.Print()
 }
